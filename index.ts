6c0463a111c87ef43eb3d87f476d0b669b14fc03
Martins100-Tmd <olaitanfabayo@gmail.com>
2025-03-06 15:35:40 +0100

    1. Added correct types to code 2. Created mongodb connection string 3. Database connected to backend and works fine

--- index.ts
+++ index.ts
@@ -1,30 +1,33 @@
-import "dotenv/config"; 
-import connectDB from "./src/config/db";
-import express, { Application, Express } from "express";
-import waitlist from "./src/router/waitlist.router";
-import admin from "./src/router/admin.router"
-import cookieParser from "cookie-parser";
+import 'dotenv/config';
+import connectDB from './src/config/db';
+import waitlist from './src/router/waitlist.router';
+import express, { Application } from 'express';
+import type { Response, Request } from 'express';
+import admin from './src/router/admin.router';
+import cookieParser from 'cookie-parser';
 
- const PORT = 3000
+const PORT = 3000;
 const app: Application = express();
 
-app.use(express.json())
-app.use(cookieParser())
+app.use(express.json());
+app.use(cookieParser());
 
-app.use('/users', waitlist)
-app.use('/admin', admin)
+app.use('/users', waitlist);
+app.use('/admin', admin);
 
-app.get('/', (req: any, res: any) => {
-     res.status(201).json({
-        message: "API is working"
-     })
-})
-
-connectDB().then(() => {
-    app.listen(3000, () => {
-        console.log('Server running on port 3000');
-    });
-}).catch(error => {
-    console.error('Failed to connect to database', error);
-    process.exit(1);
+app.get('/', (req: Request, res: Response) => {
+   res.status(201).json({
+      message: 'API is working',
+   });
 });
+
+connectDB()
+   .then(() => {
+      app.listen(3000, () => {
+         console.log('Server running on port 3000');
+      });
+   })
+   .catch((error) => {
+      console.error('Failed to connect to database', error);
+      process.exit(1);
+   });
